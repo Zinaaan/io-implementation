aa39b8e7aa629b27edb29039b33841bebd780040
Zinaaan <jiaowonanzai@gmail.com>
2023-06-20 15:48:50 -0400

    The customized implementation of bytebuffer decoder which specify the data format: length + data

---
+++ src/main/java/netty/mychat/mydecoder/CustomDecoder.java
@@ -0,0 +1,45 @@
+package netty.mychat.mydecoder;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+/**
+ * @author lzn
+ * @date 2023/06/20 14:19
+ * @description Customized message decoder
+ * <p>
+ * data format: length + data, eg: 41234
+ */
+public class CustomDecoder extends ByteToMessageDecoder {
+
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+
+        // Check whether the data is valid (the length of a int variable is 4)
+        if (byteBuf.readableBytes() < 4) {
+            return;
+        }
+
+        int length = byteBuf.readInt();
+        // If the remaining data size less than
+
+        // Typically, if the data is 41234, the <length> is 4, data is 1234, so the total expect length will be 20
+        // However, if the data is 4123, the readBytes() method will throw exception as we supposed to read 16 bytes but obviously there are only 12 bytes to read
+        // To resolve this issues we using byteBuf.readableBytes() to check whether the remaining data size is sufficient.
+        if (byteBuf.readableBytes() < length) {
+            // If the remaining data is insufficient, reset the read index to the previous position (last execution of byteBuf.markReaderIndex()) and return
+            // Why? Because this code {int length = byteBuf.readInt()} has already read 4 bytes, so we have to reset the read index to the previous position
+            byteBuf.resetReaderIndex();
+            return;
+        }
+        byte[] bytes = new byte[length];
+        byteBuf.readBytes(bytes);
+
+        // Mark the read index in order to the next reading process
+        byteBuf.markReaderIndex();
+        System.out.println(new String(bytes));
+    }
+}
---
+++ src/main/java/netty/mychat/server/DiscardServer.java
@@ -0,0 +1,61 @@
+package netty.mychat.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import netty.mychat.mydecoder.CustomDecoder;
+
+/**
+ * @author lzn
+ * @date 2023/06/18 22:24
+ * @description Netty server
+ */
+public class DiscardServer {
+
+    private final int port;
+
+    public DiscardServer(int port) {
+        this.port = port;
+    }
+
+    public void run() {
+        // Default value is twice of the number of core cpu
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new CustomDecoder());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            // Bind and start to accept incoming connections.
+            System.out.println("tcp start success");
+            ChannelFuture f = b.bind(port).sync();
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to graceful
+            // shut down your server
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+
+    }
+}
