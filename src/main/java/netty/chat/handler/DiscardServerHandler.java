13eb2e36b6c7c6de09e17967c5b420ca0cc6bdd9
Zinaaan <jiaowonanzai@gmail.com>
2023-06-20 15:47:18 -0400

    The implementation of the simulated chatting room, includes on line, send/receive message and offline

--- src/main/java/netty/chat/handler/DiscardServerHandler.java
+++ src/main/java/netty/chat/handler/DiscardServerHandler.java
@@ -1,10 +1,8 @@
 package netty.chat.handler;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.CharsetUtil;
 
 import java.util.HashSet;
 import java.util.Set;
